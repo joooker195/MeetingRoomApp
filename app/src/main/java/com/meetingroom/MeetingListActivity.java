d4acb19eba955f514b45cba33d735cea198f7324
joooker195 <joooker195@gmail.com>
2016-12-05 02:11:50 +0400

    fix settings ui

--- app/src/main/java/com/meetingroom/MeetingListActivity.java
+++ app/src/main/java/com/meetingroom/MeetingListActivity.java
@@ -9,8 +9,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -24,29 +22,9 @@ public class MeetingListActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meeting_list);
 
-        Button button = (Button) findViewById(R.id.a);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                mAuth = FirebaseAuth.getInstance();
-                mAuth.signOut();
-
-                Intent intent = new Intent(MeetingListActivity.this, MainActivity.class);
-                startActivity(intent);
-            }
-        });
-
-
-
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-    /*    DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();*/
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
@@ -71,12 +49,9 @@ public class MeetingListActivity extends AppCompatActivity
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
@@ -87,17 +62,26 @@ public class MeetingListActivity extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
+
         int id = item.getItemId();
 
-        if (id == R.id.nav_group) {
-            // Handle the camera action
-        } else if (id == R.id.nav_add_group) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        switch (id) {
+            case R.id.nav_exit:
+                mAuth = FirebaseAuth.getInstance();
+                mAuth.signOut();
+                Intent intent = new Intent(MeetingListActivity.this, MainActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.nav_add_group:
+                break;
+            case R.id.nav_group:
+                break;
+            case R.id.nav_search:
+                break;
+            case R.id.nav_settings:
+                break;
+            case R.id.nav_faq:
+                break;
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
