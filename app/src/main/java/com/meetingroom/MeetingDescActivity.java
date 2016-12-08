f8a9ac6376200ae5a5a928463f9425df7c59d9c8
joooker195 <joooker195@gmail.com>
2016-12-09 00:59:57 +0400

    fix meeting description

--- app/src/main/java/com/meetingroom/MeetingDescActivity.java
+++ app/src/main/java/com/meetingroom/MeetingDescActivity.java
@@ -3,10 +3,10 @@ package com.meetingroom;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ListView;
+import android.widget.ExpandableListView;
+import android.widget.SimpleExpandableListAdapter;
 import android.widget.TextView;
 
 import com.firebase.client.DataSnapshot;
@@ -14,24 +14,27 @@ import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
 import com.firebase.client.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 
 public class MeetingDescActivity extends AppCompatActivity {
 
 
     private Firebase mRef;
-    private Map<String, String> mMeetingPartysList;
-    private Map<String, Map<String,String>> mMeeting;
-    private Map<String,Map<String,String>> mMeetingsList;
+    private Firebase mRefListPartys;
 
     public static  String KEY = "";
 
+    private ArrayList<String> mListPartys = new ArrayList<>();
+
     private TextView mTitle;
     private TextView mDesc;
-    private ListView mPrtys;
+    private ExpandableListView mPrtys;
     private TextView mBegin;
     private TextView mEnd;
     private TextView mPriority;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,7 +47,7 @@ public class MeetingDescActivity extends AppCompatActivity {
         mBegin = (TextView) findViewById(R.id.desc_begin);
         mEnd = (TextView) findViewById(R.id.desc_end);
         mPriority = (TextView) findViewById(R.id.desc_priority);
-        mPrtys = (ListView) findViewById(R.id.desc_party);
+        mPrtys = (ExpandableListView) findViewById(R.id.desc_party);
 
 
         mRef = new Firebase("https://meeting-room-3a41e.firebaseio.com/Meetings/");
@@ -60,8 +63,56 @@ public class MeetingDescActivity extends AppCompatActivity {
                 mEnd.setText("Конец: "+ map.get("n_"+KEY).get("end").toString());
                 mPriority.setText("Приоритет: "+map.get("n_"+KEY).get("priority").toString());
 
+            }
 
-                Log.v("E_VALUE", "Message:" + KEY);
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
+
+        mRefListPartys = new Firebase("https://meeting-room-3a41e.firebaseio.com/Meetings/n_"+KEY+"/partys");
+        mRefListPartys.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Map<String, Map<String,String>> list = dataSnapshot.getValue(Map.class);
+
+                Map<String,String> mapDataList;
+                Map<String,String> mapChildDataList;
+
+                ArrayList<Map<String, String>> groupDataList = new ArrayList<>();
+                ArrayList<ArrayList<Map<String,String>>> childDataList = new ArrayList<>();
+                ArrayList<Map<String,String>> childDataItemList = new ArrayList<>();
+
+                for(int i=0; i< list.size(); i++)
+                {
+                    Map<String, String> childList = list.get("p_"+i);
+                    mapDataList = new HashMap<>();
+                    mapDataList.put("number", String.valueOf(i+1)+") " + childList.get("name"));
+
+                    groupDataList.add(mapDataList);
+
+                    mapChildDataList = new HashMap<>();
+                    mapChildDataList.put("desc", "Должность: " + childList.get("prof"));
+                    childDataItemList.add(mapChildDataList);
+
+                    childDataList.add(childDataItemList);
+                }
+
+                String groupFrom[] = new String[] {"number"};
+                int groupTo[] = new int[] {android.R.id.text1};
+
+                String childFrom[] = new String[] { "desc" };
+                int childTo[] = new int[] { android.R.id.text1 };
+
+                SimpleExpandableListAdapter adapter = new SimpleExpandableListAdapter(
+                        MeetingDescActivity.this, groupDataList,
+                        android.R.layout.simple_expandable_list_item_1, groupFrom,
+                        groupTo, childDataList, android.R.layout.simple_list_item_1,
+                        childFrom, childTo);
+
+                mPrtys.setAdapter(adapter);
+
             }
 
             @Override
